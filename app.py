041d87533bd215c59f1d3da89948137886de660b
조민수 <chominsu10@gmail.com>
2021-04-28 20:15:51 +0900

    Revert "[ADD] main PostingList"
    This reverts commit 8beff6b83ce1c7c5e6606b4d9ae98df533dafbc1.

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import *
 app = Flask(__name__)
-
 @app.route('/')
 def main():    
     return render_template('index.html')
@@ -12,10 +11,9 @@ def login():
 
 # 로그인 폼 전송을 했을때 실행될 함수
 @app.route('/login', methods=['POST'])
-def result():
+def login_post():
 #     #TODO: request.values 에서 폼에서 입력한 값을 가지고 오기
-      result = requset.form
-      return render_template('loginedHome.html')
+    
 #     #TODO: DB에서 확인
     
 #     #TODO: 아이디,비밀번호가 맞으면
@@ -24,7 +22,7 @@ def result():
 #     # 계정 정보가 안 맞다면
 #     # login.html을 다시 표시하되, 에러메시지를 포함
     
-    #  return render_template('index.html')
+    return render_template('./login.html')
 
 # # 회원가입 페이지
 @app.route('/join')
